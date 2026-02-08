647dfe3ec7be9c52699e3b799dc45b213b7d7510
Sohil Parekh <sohilparekh89@gmail.com>
2026-02-08 17:13:52 +0530

    [CLEANUP] removed unwanted files

--- jest.config.simple.js
+++
@@ -1,9 +0,0 @@
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({
-  dir: './',
-  testEnvironment: 'jest-environment-node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-})
-
-module.exports = createJestConfig
--- seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "Quotes" (text, kind, "createdAt", "updatedAt") VALUES 
-('The only way to do great work is to love what you do.', 'Opinion', NOW(), NOW()),
-('The Earth revolves around the Sun.', 'Fact', NOW(), NOW()),
-('Innovation distinguishes between a leader and a follower.', 'Opinion', NOW(), NOW()),
-('Water boils at 100 degrees Celsius at sea level.', 'Fact', NOW(), NOW()),
-('Success is not final, failure is not fatal: it is the courage to continue that counts.', 'Opinion', NOW(), NOW());
